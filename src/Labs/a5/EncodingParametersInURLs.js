223cec6af0376aafcd88cf625a93411074e937c2
NeetiKYAdhia <144548876+NeetiKYAdhia@users.noreply.github.com>
2023-12-13 15:07:16 -0500

    Update EncodingParametersInURLs.js

--- src/Labs/a5/EncodingParametersInURLs.js
+++ src/Labs/a5/EncodingParametersInURLs.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const API_BASE = process.env.REACT_APP_API_BASE;
-const URL = API_BASE?.substring(0, API_BASE.length) + '/a5';
+const URL = API_BASE?.substring(0, API_BASE.length) + 'a5';
 
 function EncodingParametersInURLs() {
   const [a, setA] = useState(34);
