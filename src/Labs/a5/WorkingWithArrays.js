0d62789d7d64ef713607f81a4e25041b20902ed8
NeetiKYAdhia <144548876+NeetiKYAdhia@users.noreply.github.com>
2023-12-13 15:07:08 -0500

    Update WorkingWithArrays.js

--- src/Labs/a5/WorkingWithArrays.js
+++ src/Labs/a5/WorkingWithArrays.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 const API_BASE = process.env.REACT_APP_API_BASE;
-const URL = API_BASE?.substring(0, API_BASE.length) + '/a5';
+const URL = API_BASE?.substring(0, API_BASE.length) + 'a5';
 function WorkingWithArrays() {
   const API = `${URL}/todos`;
   const [id, setId] = useState(0);
